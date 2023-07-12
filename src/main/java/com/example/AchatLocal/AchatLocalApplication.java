55af28bee37f706676daa6b81a47442569388d56
Nassim BenSalah <nassim.bensalah@esprit.tn>
2023-07-12 18:09:32 +0100

    init commit

---
+++ src/main/java/com/example/AchatLocal/AchatLocalApplication.java
@@ -0,0 +1,13 @@
+package com.example.AchatLocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchatLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchatLocalApplication.class, args);
+	}
+
+}
