7af4c98304d377774a1e9c428add15969c7adb3a
nassim <naseem.bensalah@gmail.com>
2023-07-13 14:16:44 +0200

    testing crud

--- src/main/java/com/example/AchatLocal/AchatLocalApplication.java
+++ src/main/java/com/example/AchatLocal/AchatLocalApplication.java
@@ -1,20 +1,13 @@
 package com.example.AchatLocal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.AchatLocal.Service.RoleService;
-
 @SpringBootApplication
 public class AchatLocalApplication {
-
-	@Autowired
-	private static RoleService serv;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AchatLocalApplication.class, args);
-		System.out.println(serv.findAll());
 	}
 
 }
